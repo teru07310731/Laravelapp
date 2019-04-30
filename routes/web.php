0b66b956dde17d4e9655cb566380991639dd7274
teru07310731 <for.wind.to.blow.tomorrow@gmail.com>
2019-04-30 10:00:17 +0900

    03 commit

--- routes/web.php
+++ routes/web.php
@@ -14,25 +14,4 @@ Route::get('/',function () {
 		return view('welcome');
 });
 
-Route::get('hello/{msg}',function($msg) {
-$html =	<<<EOF
-<html>
-	<head>
-		<title>Hello</title>
-			<style>
-				body {font-size:16pt; color:#999; }
-				h1 { font-size:100pt; text-align:right; color:#eee;
-					margin:-40px 0px -50px 0px; }
-			</style>
-	</head>
-	<body>
-		<h1>Hello</h1>
-			<p>{$msg}</P>
-		<p>This is sample page.</P>
-		<p>これは、サンプルページで作ったページです。</P>
-	</body>
-</html>
-EOF;
-
-return $html;
-});
+Route::get('hello/{id?}/{pass?}', 'HelloController@index');
