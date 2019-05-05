6f55565fc7cfdd5ee286173ecce18258410e2ba4
teru07310731 <for.wind.to.blow.tomorrow@gmail.com>
2019-05-05 11:41:45 +0900

    51 commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,17 +1,17 @@
+<!DOCTYPE html>
 <html lang="ja">
 <head>
 	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible content=IE=edge">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=devide-width, initial-scale=1">
 
 	<title>Book app</title>
 
 	<link href="https://fonts.googlepist.com/css/?family=Railway:300,400,500,700" rel="stylesheet" type="text/css">
-	<link href="https://maxcdn.bootstrap.com"/font-awesome/4.4.0/css/font-awsome.min.css" rel="stylesheet" type="text/css">
-	<link href="https://maxcdn.bootstrap.com/">
-
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.min.js"></script>
-	<script src="https://maxcdn.bootstrap.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0-alpha1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 
 	<style>
 		body {
@@ -36,21 +36,23 @@
 </head>
 <body>
 	<div class="cotainar">
-		<nav class="coteinar-fluid">
-			<div class="navbar-header">
-				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-toggle"navbar" aria-expanded="false" aria-controls="navbar">
-					<span class="sr-only">Toggle navigation</span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
+		<nav class="navbar navbar-default">
+			<div class="coteinar-fluid">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
 
-				<a class="navbar-bland href="#">Reading List</a>
-			</div>
-			<div class="navbar" class="navbar-collapse collapse">
-				<ul class="nav navbar-nav">
-					&nbsp;
-				</ul>
+					<a class="navbar-brand" href="#">Reading List</a>
+				</div>
+				<div id="navbar" class="navbar-collapse collapse">
+					<ul class="nav navbar-nav">
+						&nbsp;
+					</ul>
+				</div>
 			</div>
 		</nav>
 	</div>
